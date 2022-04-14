f8ec78fb37a591750625cef9c34a6e1f724cdcf7
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-04-14 11:35:09 +0300

    button

--- src/components/Etusivu.js
+++ src/components/Etusivu.js
@@ -4,44 +4,44 @@ import { Button } from '@mui/material';
 
 function Kysely() {
 
-    const [listkysely, setListKysely] = useState([]);
-    const [kysely, setKysely] = useState([]);
-    const [kyselyt, setKyselyt] = useState([]);
+   const [listkysely, setListKysely] = useState([]);
+   const [kysely, setKysely] = useState([]);
+   const [kyselyt, setKyselyt] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:8080/kyselyt')
-            .then(res => res.json())
-            .then(items => {
-                setKyselyt(items)
-            })
-            .catch(err => console.error(err))
-    }, []);
+   useEffect(() => {
+      fetch('http://localhost:8080/kyselyt')
+         .then(res => res.json())
+         .then(items => {
+            setKyselyt(items)
+         })
+         .catch(err => console.error(err))
+   }, []);
 
 
-    return (
-        <div>
-            <h1>ETUSIVU</h1>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Nimi</th>
-                        <th>Kuvaus</th>
-                        <th>Vastaa</th>
-                    </tr>
-                    {
-                        kyselyt.map((kysely, index) =>
-                            <tr key={index}>
-                                <td>{kysely.nimi}</td>
-                                <td>{kysely.kuvaus}</td>
-                                <td><Button color="success" variant="contained">Vastaa</Button></td>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
+   return (
+      <div>
+         <h1>ETUSIVU</h1>
+         <table>
+            <tbody>
+               <tr>
+                  <th>Nimi</th>
+                  <th>Kuvaus</th>
+                  <th>Vastaa</th>
+               </tr>
+               {
+                  kyselyt.map((kysely, index) =>
+                     <tr key={index}>
+                        <td>{kysely.nimi}</td>
+                        <td>{kysely.kuvaus}</td>
+                        <td><Button color="success" variant="contained">Vastaa</Button></td>
+                     </tr>
+                  )
+               }
+            </tbody>
+         </table>
 
-        </div>
-    )
+      </div>
+   )
 }
 
 export default Kysely;
