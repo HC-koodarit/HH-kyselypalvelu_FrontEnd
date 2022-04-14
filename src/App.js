66ed8a5dc2c8603bb1df3e257dd133b2dcba39b2
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-04-14 10:22:36 +0300

    routerjuttui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <KatsoKysely />
+    <div className="App">
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Router>
+      <Toolbar>
+        <Link className="link" to="/">Customers</Link>
+        <Link className="link" to="/traininglist">Traininglist</Link>
+        <Link className="link" to="/calendar">Calendar</Link>
+      </Toolbar>
+      <Routes>
+        <Route exact path="/" element={<Customerlist />} />
+        <Route path="/traininglist" element={<Traininglist />} />
+        <Route path="/calendar" element={<MyCalendar />} />
+        <Route render = {() => <h1>Page not found</h1>} />
+      </Routes>
+      </Router>
+    </AppBar>
+  </Box>
+</div>
   );
 }
 
