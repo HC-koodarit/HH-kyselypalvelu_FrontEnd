ddc020fa3c65273108366fcf939288f5d0d1f337
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-04-14 10:37:12 +0300

    etusivu

---
+++ src/components/Etusivu.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+function Kysely() {
+    const [listkysely, setListKysely] = useState([]);
+    const [kysely, setKysely] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/kyselyt/1')
+        .then(res => res.json())
+        .then(items => {
+            setListKysely(items)
+        })
+        .catch(err =>console.error(err))
+    }, []);
+
+    return(
+        <div>
+            <h1>ETUSIVU</h1>
+            <p>{listkysely.id}</p>
+            <p>{listkysely.nimi}</p>
+                {
+                listkysely.map((kysely, index) =>
+                <ul key={index}>{kysely.kysely.nimi}
+                <li>{kysely.kysymysteksti}</li>
+                </ul>
+                )
+                }
+            
+        </div>
+    )
+}
+
+export default Kysely;
